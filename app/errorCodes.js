a71fb74c8c8acee896e84f2a966e3a23d239fe8a
Jarod Brennfleck <robinjars@gmail.com>
2020-09-09 20:12:19 +1000

    error codes

---
+++ app/errorCodes.js
@@ -0,0 +1,30 @@
+module.exports = {
+    400: {
+        statusCode: 400,
+        msg: "Bad request."
+    },
+    401: {
+        statusCode: 401,
+        msg: "Unauthorized access."
+    },
+    403: {
+        statusCode: 403,
+        msg: "Forbidden."
+    },
+    404: {
+        statusCode: 404,
+        msg: "Not found."
+    },
+    405: {
+        statusCode: 405,
+        msg: "Method not allowed."
+    },
+    500: {
+        statusCode: 500,
+        msg: "Internal server error."
+    },
+    501: {
+        statusCode: 501,
+        msg: "Not implemented."
+    },
+};
