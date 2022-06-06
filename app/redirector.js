b157527995342d67a1cf8045072d4eb668116790
TheBrenny <iam@justbrenny.me>
2022-06-06 22:27:02 +0930

    enhanced redirector

--- app/redirector.js
+++ app/redirector.js
@@ -2,12 +2,13 @@ const hostsJson = require("./api/hosts-json");
 
 module.exports = (router) => {
     router.get(".*", async function (req, res) {
-        if (req.host === "hostman") return;
+        if(req.host === "hostman") return;
         let hosts = await hostsJson.get();
         let host = hosts.find(h => h.host === req.host);
+        let hasProtocol = host.address.includes("://");
 
         res.writeHead(302, {
-            'Location': 'https://' + host.address
+            'Location': (!hasProtocol ? 'https://' : "") + host.address
         });
         res.end();
     });
