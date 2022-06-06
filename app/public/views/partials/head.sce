4a62d9465354cbd068e411bae5b1d112a362a4c6
TheBrenny <iam@justbrenny.me>
2022-06-06 22:26:51 +0930

    added better favicon

--- app/public/views/partials/head.sce
+++ app/public/views/partials/head.sce
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>hostman</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nova+Mono&display=swap">
-    <link rel="stylesheet" href="/assets/css/main.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon/favicon-16x16.png">
-    <link rel="manifest" href="/assets/img/favicon/site.webmanifest">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <title>hostman</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nova+Mono&amp;display=swap">
+        <link rel="stylesheet" href="/assets/css/main.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/favicon/apple-touch-icon.png?v=1">
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon/favicon-32x32.png?v=1">
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon/favicon-16x16.png?v=1">
+        <link rel="manifest" href="/assets/img/favicon/site.webmanifest?v=1">
+        <link rel="mask-icon" href="/assets/img/favicon/safari-pinned-tab.svg?v=1" color="#5bbad5">
+        <link rel="shortcut icon" href="/assets/img/favicon/favicon.ico?v=1">
+        <meta name="apple-mobile-web-app-title" content="Hostman">
+        <meta name="application-name" content="Hostman">
+        <meta name="msapplication-TileColor" content="#ceeefd">
+        <meta name="msapplication-config" content="/assets/img/favicon/browserconfig.xml?v=1">
+        <meta name="theme-color" content="#ffffff">
+    </head>
 
 <body>
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,99 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 const nodemon = require('gulp-nodemon');
+const realFavicon = require('gulp-real-favicon');
+const fs = require("fs");
+
+const FAVICON_DATA_FILE = 'faviconData.json';
+
+gulp.task("generate-favicon", (done) => {
+    realFavicon.generateFavicon({
+        masterPicture: './app/public/assets/img/favicon/favicon.svg',
+        dest: './app/public/assets/img/favicon/',
+        iconsPath: '/assets/img/favicon',
+        design: {
+            ios: {
+                pictureAspect: 'backgroundAndMargin',
+                backgroundColor: '#ceeefd',
+                margin: '14%',
+                assets: {
+                    ios6AndPriorIcons: false,
+                    ios7AndLaterIcons: false,
+                    precomposedIcons: false,
+                    declareOnlyDefaultIcon: true
+                },
+                appName: 'Hostman'
+            },
+            desktopBrowser: {
+                design: 'raw'
+            },
+            windows: {
+                pictureAspect: 'noChange',
+                backgroundColor: '#ceeefd',
+                onConflict: 'override',
+                assets: {
+                    windows80Ie10Tile: false,
+                    windows10Ie11EdgeTiles: {
+                        small: true,
+                        medium: true,
+                        big: true,
+                        rectangle: false
+                    }
+                },
+                appName: 'Hostman'
+            },
+            androidChrome: {
+                pictureAspect: 'noChange',
+                themeColor: '#ffffff',
+                manifest: {
+                    name: 'Hostman',
+                    display: 'browser',
+                    orientation: 'notSet',
+                    onConflict: 'override',
+                    declared: true
+                },
+                assets: {
+                    legacyIcon: false,
+                    lowResolutionIcons: false
+                }
+            },
+            safariPinnedTab: {
+                pictureAspect: 'silhouette',
+                themeColor: '#5bbad5'
+            }
+        },
+        settings: {
+            scalingAlgorithm: 'Mitchell',
+            errorOnImageTooSmall: false,
+            readmeFile: false,
+            htmlCodeFile: true,
+            usePathAsIs: false
+        },
+        versioning: {
+            paramName: 'v',
+            paramValue: '1'
+        },
+        markupFile: FAVICON_DATA_FILE
+    }, function () {
+        done();
+    });
+});
+
+gulp.task('inject-favicon-markups', async function () {
+    const code = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code;
+    
+
+    return gulp.src(['./app/public/views/partials/head.sce'])
+        .pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
+        .pipe(gulp.dest('./app/public/views/partials/'));
+});
+
+gulp.task('check-for-favicon-update', function (done) {
+    var currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
+    realFavicon.checkForUpdates(currentVersion, function (err) {
+        if(err) throw err;
+    });
+});
 
 gulp.task("sass", function () {
     return gulp.src("app/public/assets/scss/**/*.scss")
@@ -28,12 +121,13 @@ gulp.task("nodemon", function (cb) {
         script: 'hostman.js',
         env: {
             "NODE_ENV": 'dev',
-            "GULPING": true
+            "GULPING": true,
+            "HOSTMAN_HOST": "hostmandev"
         },
     }).on('start', function () {
         // to avoid nodemon being started multiple times
         // thanks @matthisk
-        if (!started) {
+        if(!started) {
             started = true;
             console.log("Nodemon started.");
             cb();
